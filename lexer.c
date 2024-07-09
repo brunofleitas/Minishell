94ad0e1728c5395c2fd5b1badcf544e8b28fa3c8
Bruno Fleitas Martinez <bfleitas@c2r10s4.42luxembourg.lu>
2024-07-09 17:57:52 +0200

    lexer first try

--- lexer.c
+++ lexer.c
@@ -6,7 +6,7 @@
 /*   By: bfleitas <bfleitas@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 15:03:22 by bfleitas          #+#    #+#             */
-/*   Updated: 2024/07/09 17:44:29 by bfleitas         ###   ########.fr       */
+/*   Updated: 2024/07/09 17:52:57 by bfleitas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void    lexer(char *input)
     split = ft_split(input, ' ');
     while (split[i])
     {
+        tokens[i] = malloc(sizeof(t_word));
         tokens[i]->value = split[i];
         //tokens[i]->key = get_key(tokens[i]->value);
         printf("%s\n", tokens[i]->value);
